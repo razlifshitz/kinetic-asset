65bcac25ff4392c2bad3c4673761e5ea6aba86e0
raz.lifshitz <raz.lifshitz@appsflyer.com>
2023-07-25 22:40:35 +0300

    fix head

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,23 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kinetic Asset</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <div className='headerContainer'>
+        <div className="headerContainer">
           <h1 className="title">Kinetic Asset</h1>
-          <p className="description">
-            Enabling access to illiquid assets
-          </p>
+          <p className="description">Enabling access to illiquid assets</p>
         </div>
-        
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
